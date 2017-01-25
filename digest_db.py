84affaa78200b3e383bede2a289fd3f757f36e54
Peter Mitrano <mitranopeter@gmail.com>
2017-01-25 14:45:45 -0500

    show how to acces "midi data" for an individual solo

--- digest_db.py
+++ digest_db.py
@@ -4,21 +4,30 @@ from collections import namedtuple
 import sqlite3
 
 
-SoloMetadata = namedtuple('SoloMetadata', ['melid', 'tempoclass', 'signature', 'chord_changes'])
+SoloMetadata = namedtuple('SoloMetadata', ['melid', 'instrument', 'tempoclass', 'signature', 'chord_changes'])
+legal_instruments = ['ts', 'as', 'bs', 'tp', 'tb', 'ts-c', 'cor', 'ss']
 
 conn = sqlite3.connect('wjazzd.db')
 
 c = conn.cursor()
-keys = ', '.join(SoloMetadata._fields)
-print(keys)
-solos = c.execute('select %s from solo_info' % keys)
+solo_table_keys = ', '.join(SoloMetadata._fields)
+solos = c.execute('select %s from solo_info' % solo_table_keys)
 
 # filter out bad entries
 valid_solos = []
 for row in solos:
     solo = SoloMetadata(*row)
-    if solo.melid and solo.tempoclass and solo.signature and solo.chord_changes:
+
+    if solo.melid and solo.tempoclass and solo.signature \
+       and solo.chord_changes and solo.instrument in legal_instruments:
         valid_solos.append(solo)
 
+# collect midi
+MelodyMetadata = namedtuple('MelodyMetadata', ['melid', 'eventid', 'onset', 'pitch', 'duration', 'loud_cent'])
+melody_table_keys = ', '.join(MelodyMetadata._fields)
+demo_solo = valid_solos[0]
+melody = c.execute('select %s from melody where melid=?' % melody_table_keys, str(demo_solo.melid))
 
-print(len(valid_solos), 'valid solo.')
+for row in melody:
+    event = MelodyMetadata(*row)
+    print(event)
