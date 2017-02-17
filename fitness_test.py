bb5386ba729e217acf19e44d36a1d0279144d895
Peter Mitrano <mitranopeter@gmail.com>
2017-02-17 18:35:31 -0500

    ok so it likes apreggio's more then randomness

--- fitness_test.py
+++ fitness_test.py
@@ -8,6 +8,7 @@ import music21
 import fitness
 
 ff = fitness.FitnessFunction()
+N = 20
 
 random_fitness = []
 def random_midi(ff):
@@ -24,24 +25,44 @@ def random_midi(ff):
     mf.close()
     return ff.evaluate_fitness(".temp.mid")
 
-for i in range(100):
+for i in range(20):
     f, l = random_midi(ff)
-    print(f/l)
     random_fitness.append(f/l)
 
-print('\n\n\n')
 jazz_fitness = []
 for filename in os.listdir("jazz_midis"):
     if filename.endswith(".mid"):
         try:
             f, l = ff.evaluate_fitness("jazz_midis/" + filename)
-            print(f/l)
             jazz_fitness.append(f/l)
         except Exception:
-            print(filename + " is invalid")
+            #print(filename + " is invalid")
+            pass
+    if len(jazz_fitness) == N:
+        break
+
+arpeggio_fitness = []
+for tonic in range(0, N):
+    s = music21.stream.Stream()
+    s.append(music21.note.Note(tonic))
+    s.append(music21.note.Note(tonic + 4))
+    s.append(music21.note.Note(tonic + 7))
+    s.append(music21.note.Note(tonic + 12))
+    s.append(music21.note.Note(tonic + 16))
+    s.append(music21.note.Note(tonic + 12))
+    s.append(music21.note.Note(tonic + 7))
+    s.append(music21.note.Note(tonic + 4))
+
+    mf = music21.midi.translate.streamToMidiFile(s)
+    mf.open('.temp.mid', 'wb')
+    mf.write()
+    mf.close()
+    f, l = ff.evaluate_fitness(".temp.mid")
+    arpeggio_fitness.append(f/l)
 
 
 plt.plot(random_fitness, label='random')
+plt.plot(arpeggio_fitness, label='arpeggio')
 plt.plot(jazz_fitness, label='real jazz')
 plt.legend()
 plt.show()
