32439544ccb29f3ff6e2d3d71eb6467fd1b8f949
Peter Mitrano <mitranopeter@gmail.com>
2017-02-22 23:55:03 -0500

    starting analysis of manual training logs

---
+++ analyze_feedback.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+
+def main():
+    with open('feedback/peter_manual_1_measure_feedback.csv', 'rb') as csv_file:
+        reader = csv.reader(csv_file, delimiter=',')
+        for row in reader:
+            print(row)
+
+
+
+    # scatter plot of the feedback given to each measure
+
+
+
+if __name__ == '__main__':
+    main()
