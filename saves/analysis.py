faadf47e02ff5952372e05fe2ed5e433510970f7
Peter Mitrano <mitranopeter@gmail.com>
2017-03-02 15:13:53 -0500

    working on improving analysis of population data

--- saves/analysis.py
+++ saves/analysis.py
@@ -2,46 +2,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-if len(sys.argv) != 2:
-    print("USAGE: python analysis.py gen_number")
-    sys.exit()
+measure_pop_size = 32
+phrase_pop_size = 48
+max_gen = 15
 
-gen_num = sys.argv[1]
-pfile = 'phrases_' + gen_num + '.np'
-mfile = 'measures_' + gen_num + '.np'
-
-def pop_hist(filename, title, bins):
+counts_over_time = np.ndarray((measure_pop_size, max_gen))
+for gen_num in range(0, max_gen):
+    filename = 'phrases_%i.np' % gen_num
     pop_f = open(filename,'r')
     pop = [l.strip('\n') for l in pop_f.readlines()]
-    genes = []
+    counts = np.zeros(measure_pop_size)
     for genome in pop:
-        genes += [int(g) for g in genome.split(' ')[:-1]]
-    plt.figure()
-    plt.title(title)
-    plt.hist(genes, bins=bins)
+        # -1 is to ignore fitness
+        for g in genome.split(' ')[:-1]:
+            counts[int(g)] += 1
+    counts_over_time[:,gen_num] = counts
+    print(np.sum(counts))
+plt.figure()
+plt.stackplot(np.arange(max_gen), *counts_over_time)
+plt.xlabel('measures used in phrase population')
+plt.savefig('phrase_distrobution.png')
 
-
-pop_hist(pfile, 'Phrases', 64)
-pop_hist(mfile, 'Measures', 16)
-
-
-# # Distrobutions of Pitches
-def pitch_hist(filename, title):
-    pop_f = open(filename,'r')
-    pop = [l.strip('\n') for l in pop_f.readlines()]
-    pitches = []
-    for genome in pop:
-        for g_s in genome.split(' ')[:-1]:
-            g_i = int(g_s)
-            if 1 < g_i < 15:
-                pitches.append(g_i)
-    plt.figure()
-    plt.title(title)
-    plt.hist(pitches)
-
-
-pitch_hist(mfile, 'Pitches from 1-15 Used')
-
+# TODO: measures
+mfile = 'measures_%i.np' % gen_num
 
 # # Distrobution of Durations
 def duration_hist(filename, title):
@@ -53,20 +36,19 @@ def duration_hist(filename, title):
     for genome in pop:
         for g_s in genome.split(' ')[:-1]:
             g_i = int(g_s)
-            if g_i == 0:
-                if duration != -1:
+            if 0 <= g_i < 15:
+                if note_on:
                     durations.append(duration)
-                note_on = False
-                duration = 0
-            elif 1 < g_i < 15:
+                duration = 1
                 note_on = True
             elif note_on and g_i == 15:
                 duration += 1
 
     plt.figure()
-    plt.title(title)
-    plt.hist(durations)
+    plt.hist(durations, bins=list(range(1,8)))
+    plt.xlabel("Durations in number of 8th notes")
+    plt.savefig(gen_num + '_durations_used.png')
 
-duration_hist(mfile, 'Durations Used')
+#duration_hist(mfile, 'Durations Used')
 
 plt.show()
